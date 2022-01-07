c6e9e841edb32dac3136b755d1215aeabb9c1270
harmonly <bullniumao@outlook.com>
2022-01-07 14:27:25 +0800

    upload S1614.cpp

---
+++ src/S1614.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+   public:
+    int maxDepth(string s) {
+        int cnt = 0;
+        int ans = 0;
+        for (char ch : s) {
+            if (ch == '(') {
+                cnt++;
+                ans = max(ans, cnt);
+            } else if (ch == ')')
+                cnt--;
+        }
+        return ans;
+    }
+};
