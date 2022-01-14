c189a32a8dcc9febd170ce662866d94a9bb07228
harmonly <bullniumao@outlook.com>
2022-01-15 00:48:31 +0800

    update S1716.cpp

--- src/S1716.cpp
+++ src/S1716.cpp
@@ -13,4 +13,10 @@ class Solution {
         ans += k * (a1 + an) / 2;
         return ans;
     }
+
+    int totalMoney2(int n) {
+        int a = n / 7;
+        int b = n % 7;
+        return (a * (7 * a + 49) + b * (b + 1)) / 2 + b * a;
+    }
 };
