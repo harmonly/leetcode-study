df74106cf5cea912c32c94d0e7b5a28654e66a8b
harmonly <bullniumao@outlook.com>
2022-01-22 15:03:33 +0800

    upload S1332.cpp

---
+++ src/S1332.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+   public:
+    int removePalindromeSub(string s) {
+        int l = 0, r = s.size() - 1;
+        while (l < r)
+            if (s[l++] != s[r--]) return 2;
+        return 1;
+    }
+};
