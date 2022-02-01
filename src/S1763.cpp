0d19b4748342a2d33cedcbee6637d35a2abc9f9c
harmonly <bullniumao@outlook.com>
2022-02-01 21:20:22 +0800

    update S1763.cpp

--- src/S1763.cpp
+++ src/S1763.cpp
@@ -23,6 +23,27 @@ class Solution {
         }
         return ans;
     }
+
+    string longestNiceSubstring2(string s) {
+        int n = s.length();
+        int maxPos = 0;
+        int maxLen = 0;
+        for (int i = 0; i < n; ++i) {
+            int lower = 0;
+            int upper = 0;
+            for (int j = i; j < n; ++j) {
+                if (islower(s[j]))
+                    lower |= 1 << (s[j] - 'a');
+                else
+                    upper |= 1 << (s[j] - 'A');
+                if (lower == upper && j - i + 1 > maxLen) {
+                    maxPos = i;
+                    maxLen = j - i + 1;
+                }
+            }
+        }
+        return s.substr(maxPos, maxLen);
+    }
 };
 
 int main() {
