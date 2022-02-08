ea08a53a8cbec0d634f8c7c29d8ec3c5efd45db5
harmonly <bullniumao@outlook.com>
2022-02-08 14:40:49 +0800

    update util.cpp

--- util.cpp
+++ util.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-string convertBracket2Brace(string s) {
+string bracket2Brace(string s) {
     for (int i = 0; i < s.length(); ++i) {
         if (s[i] == '[')
             s[i] = '{';
@@ -12,10 +12,23 @@ string convertBracket2Brace(string s) {
     return s;
 }
 
+string convertGrid(string s) {
+    for (int i = 0; i < s.length(); ++i) {
+        if (s[i] == '[')
+            s[i] = '{';
+        else if (s[i] == ']')
+            s[i] = '}';
+        else if (s[i] == '\"')
+            s[i] = '\'';
+    }
+    return s;
+}
+
 int main() {
     int op = -1;
     cout << "------------------" << endl
          << "1. 中括号转大括号" << endl
+         << "2. 二维字符数组" << endl
          << "请输入操作数: ";
     cin >> op;
     switch (op) {
@@ -23,7 +36,15 @@ int main() {
             cout << "输入待转换字符串: " << endl;
             string s;
             cin >> s;
-            cout << convertBracket2Brace(s) << endl;
+            cout << bracket2Brace(s) << endl;
+            cout << "转换完成" << endl;
+            break;
+        }
+        case 2: {
+            cout << "输入待转换字符串: " << endl;
+            string s;
+            cin >> s;
+            cout << convertGrid(s) << endl;
             cout << "转换完成" << endl;
             break;
         }
