593411e745c908b3a6b69e783116843edc170dac
harmonly <bullniumao@outlook.com>
2022-02-09 23:49:54 +0800

    update S3.cpp

--- src/S3.cpp
+++ src/S3.cpp
@@ -2,35 +2,27 @@
 
 using namespace std;
 
-class Solution
-{
-public:
-    int lengthOfLongestSubstring(string s)
-    {
-        int n = s.length();
-        int ans = 0, right = -1;
-        unordered_set<char> set;
-        for (int i = 0; i < n; i++)
-        {
-            if (i != 0)
-                set.erase(s[i - 1]);
-            while (right + 1 < n && !set.count(s[right + 1]))
-                set.insert(s[right++ + 1]);
-            ans = max(ans, right - i + 1);
+class Solution {
+   public:
+    int lengthOfLongestSubstring(string s) {
+        int ans = 0, n = s.length();
+        unordered_set<char> st;
+        int pre = 0;
+        for (int i = 0; i < n; ++i) {
+            while (st.find(s[i]) != st.end()) st.erase(s[pre++]);
+            st.insert(s[i]);
+            ans = max(ans, i - pre + 1);
         }
         return ans;
     }
 
-    int lengthOfLongestSubstring2(string s)
-    {
+    int lengthOfLongestSubstring2(string s) {
         int set[128];
-        for (int i = 0; i < 128; i++)
-            set[i] = -1;
+        for (int i = 0; i < 128; i++) set[i] = -1;
 
         int n = s.length();
         int res = 0, start = 0;
-        for (int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++) {
             int index = s[i];
             start = max(start, set[index] + 1);
             res = max(res, i - start + 1);
