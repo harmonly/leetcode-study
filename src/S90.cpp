f3bafa5e224ff2beaa00d380c748bd3fd2613bc2
harmonly <bullniumao@outlook.com>
2022-02-11 21:03:05 +0800

    update S90.cpp

--- src/S90.cpp
+++ src/S90.cpp
@@ -8,6 +8,7 @@ class Solution {
 
    public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
         int n = nums.size();
         for (int mask = 0; mask < (1 << n); ++mask) {
             path.clear();
