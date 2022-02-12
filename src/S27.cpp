4073fe172eb5e71134a118a1cb3a40d71278b232
harmonly <bullniumao@outlook.com>
2022-02-12 22:05:59 +0800

    update S27.cpp

--- src/S27.cpp
+++ src/S27.cpp
@@ -11,6 +11,14 @@ class Solution {
         }
         return cnt;
     }
+
+    int removeElement2(vector<int>& nums, int val) {
+        int j = nums.size() - 1;
+        for (int i = 0; i <= j; ++i) {
+            if (nums[i] == val) swap(nums[i--], nums[j--]);
+        }
+        return ++j;
+    }
 };
 
 int main() {
