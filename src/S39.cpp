7192390978e661902ddc0ddd36630d90b2a61ece
harmonly <bullniumao@outlook.com>
2022-02-12 17:49:17 +0800

    upload S39.cpp

---
+++ src/S39.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+    int n;
+    vector<vector<int>> ans;
+    vector<int> path;
+
+   public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        n = candidates.size();
+        backtrack(candidates, 0, target);
+        return ans;
+    }
+
+    void backtrack(vector<int>& nums, int idx, int target) {
+        if (target == 0) {
+            if (path.size()) ans.emplace_back(path);
+            return;
+        } else if (target < 0)
+            return;
+        for (int i = idx; i < n; ++i) {
+            path.emplace_back(nums[i]);
+            backtrack(nums, i, target - nums[i]);
+            path.pop_back();
+        }
+    }
+};
