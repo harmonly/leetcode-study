b0d734469a99dde5a09d8345e919242de43cfaa4
harmonly <bullniumao@outlook.com>
2022-02-13 23:57:58 +0800

    update S39.cpp

--- src/S39.cpp
+++ src/S39.cpp
@@ -17,13 +17,14 @@ class Solution {
 
     void backtrack(vector<int>& nums, int idx, int target) {
         if (target == 0) {
-            if (path.size()) ans.emplace_back(path);
+            ans.emplace_back(path);
             return;
         } else if (target < 0)
             return;
         for (int i = idx; i < n; ++i) {
             path.emplace_back(nums[i]);
-            backtrack(nums, i, target - nums[i]);
+            backtrack(nums, i, target - nums[i]);  // 回溯时可以多次选择同一个数,
+                                                   // 此处 idx 传入 i 而非 i + 1
             path.pop_back();
         }
     }
