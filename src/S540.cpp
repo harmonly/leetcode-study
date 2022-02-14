4d15d3d91fe5aaae45ce2cf894df522202ffbd2a
harmonly <bullniumao@outlook.com>
2022-02-14 16:17:19 +0800

    update S540.cpp

--- src/S540.cpp
+++ src/S540.cpp
@@ -17,4 +17,16 @@ class Solution {
         }
         return nums[l];
     }
+
+    int singleNonDuplicate2(vector<int>& nums) {
+        int l = 0, r = nums.size() - 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] != nums[mid ^ 1])
+                r = mid;
+            else
+                l = mid + 1;
+        }
+        return nums[l];
+    }
 };
