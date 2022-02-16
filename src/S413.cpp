f20966881b2aa67ad3e26ab9ed9876f6b20382cc
harmonly <bullniumao@outlook.com>
2022-02-16 18:10:20 +0800

    update S413.cpp

--- src/S413.cpp
+++ src/S413.cpp
@@ -6,15 +6,13 @@ class Solution {
    public:
     int numberOfArithmeticSlices(vector<int>& nums) {
         int n = nums.size();
-        if (n < 3) return 0;
-        vector<int> dp(n, 0);
-        for (int i = 2; i < n; i++) {
+        int dp[n];
+        memset(dp, 0, sizeof(dp));
+        for (int i = 2; i < n; ++i) {
             if (nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2])
                 dp[i] = dp[i - 1] + 1;
-            else
-                dp[i] = 0;
         }
-        return accumulate(dp.begin(), dp.end(), 0);
+        return accumulate(dp, dp + n, 0);
     }
 
     int numberOfArithmeticSlices2(vector<int>& nums) {
