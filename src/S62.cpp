0948504e432668ac1646ff3a8fa85c06f6ed1696
harmonly <bullniumao@outlook.com>
2022-02-16 16:51:39 +0800

    update S62.cpp

--- src/S62.cpp
+++ src/S62.cpp
@@ -15,9 +15,9 @@ class Solution {
 
     int uniquePaths2(int m, int n) {
         long long ans = 1;
-        // 从左上角到右下角的过程中，我们需要移动 m+n-2m+n−2 次，其中有 m-1m−1
-        // 次向下移动，n-1n−1 次向右移动。因此路径的总数，就等于从 m+n-2m+n−2
-        // 次移动中选择 m-1m−1 次向下移动的方案数
+        // 从左上角到右下角的过程中，我们需要移动 m+n−2 次，其中有 m-1
+        // 次向下移动，n-1 次向右移动。因此路径的总数，就等于从 m+n-2
+        // 次移动中选择 m-1 次向下移动的方案数
         for (int x = n, y = 1; y < m; ++x, ++y) ans = ans * x / y;
         return ans;
     }
