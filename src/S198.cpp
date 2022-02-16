be3454c89629f8eca688c08a7bb8b552ea95a7fa
harmonly <bullniumao@outlook.com>
2022-02-16 16:41:09 +0800

    update S198.cpp

--- src/S198.cpp
+++ src/S198.cpp
@@ -14,4 +14,16 @@ class Solution {
         }
         return dp[n - 1];
     }
+
+    int rob2(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return nums[0];
+        int pre = nums[0], ans = max(nums[0], nums[1]);
+        for (int i = 2; i < n; ++i) {
+            int tmp = ans;
+            ans = max(ans, pre + nums[i]);
+            pre = tmp;
+        }
+        return ans;
+    }
 };
