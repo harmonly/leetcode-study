e50822dfef7ef17d3e0159eb3d9a55664c7c1855
harmonly <bullniumao@outlook.com>
2022-02-17 20:16:29 +0800

    update S91.cpp

--- src/S91.cpp
+++ src/S91.cpp
@@ -17,6 +17,20 @@ class Solution {
         }
         return dp[n];
     }
+
+    int numDecodings2(string s) {
+        int n = s.length();
+        int a = 0, b = 1, c;  // a = f[i-2], b = f[i-1], c = f[i]
+        for (int i = 1; i <= n; ++i) {
+            c = 0;
+            if (s[i - 1] != '0') c += b;
+            if (i > 1 && s[i - 2] != '0' &&
+                (s[i - 2] - '0') * 10 + s[i - 1] - '0' <= 26)
+                c += a;
+            a = b, b = c;
+        }
+        return c;
+    }
 };
 
 int main() {
