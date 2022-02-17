351a172ea602e0b4d5a29c2a0d650b165ec28d29
harmonly <bullniumao@outlook.com>
2022-02-17 20:45:40 +0800

    upload S139.cpp

---
+++ src/S139.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+   public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> st(wordDict.begin(), wordDict.end());
+        int n = s.length();
+        bool dp[n + 1];
+        memset(dp, 0, sizeof(dp));
+        dp[0] = true;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (dp[j] && st.find(s.substr(j, i - j)) != st.end()) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+};
