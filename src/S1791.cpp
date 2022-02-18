323c33c1916e479c81b03415d6aab10446394082
harmonly <bullniumao@outlook.com>
2022-02-18 16:57:20 +0800

    update S1791.cpp

--- src/S1791.cpp
+++ src/S1791.cpp
@@ -5,6 +5,18 @@ using namespace std;
 class Solution {
    public:
     int findCenter(vector<vector<int>>& edges) {
+        int n = edges.size();
+        vector<int> degrees(n + 2);
+        for (int i = 0; i < n; ++i) {
+            degrees[edges[i][0]]++;
+            degrees[edges[i][1]]++;
+        }
+        for (int i = 1; i <= n + 1; ++i)
+            if (degrees[i] == n) return i;
+        return 0;
+    }
+
+    int findCenter2(vector<vector<int>>& edges) {
         int a = edges[0][0], b = edges[0][1];
         int u = edges[1][0], v = edges[1][1];
         if (a == u || a == v) return a;
