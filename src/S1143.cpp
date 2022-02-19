b58a5110e8b446ea1665233c029e14f982b03b56
harmonly <bullniumao@outlook.com>
2022-02-19 19:03:44 +0800

    upload S1143.cpp

---
+++ src/S1143.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+   public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n1 = text1.length(), n2 = text2.length();
+        int dp[n1 + 1][n2 + 1], ans = 0;
+        memset(dp, 0, sizeof(dp));
+        for (int i = 1; i <= n1; ++i) {
+            for (int j = 1; j <= n2; ++j) {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                if (text1[i - 1] == text2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                ans = max(ans, dp[i][j]);
+            }
+        }
+        return ans;
+    }
+};
