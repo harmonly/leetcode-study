7e37c180f726ce9ea39de8946179a2d71433467a
harmonly <bullniumao@outlook.com>
2022-02-19 19:11:50 +0800

    upload S1143.cp

--- src/S1143.cpp
+++ src/S1143.cpp
@@ -6,16 +6,15 @@ class Solution {
    public:
     int longestCommonSubsequence(string text1, string text2) {
         int n1 = text1.length(), n2 = text2.length();
-        int dp[n1 + 1][n2 + 1], ans = 0;
+        int dp[n1 + 1][n2 + 1];
         memset(dp, 0, sizeof(dp));
         for (int i = 1; i <= n1; ++i) {
             for (int j = 1; j <= n2; ++j) {
                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
                 if (text1[i - 1] == text2[j - 1])
                     dp[i][j] = dp[i - 1][j - 1] + 1;
-                ans = max(ans, dp[i][j]);
             }
         }
-        return ans;
+        return dp[n1][n2];
     }
 };
