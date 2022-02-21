8094970315d5f7a331cf1f1570089a0373f8c95a
harmonly <bullniumao@outlook.com>
2022-02-21 16:06:36 +0800

    update S322.cpp

--- src/S322.cpp
+++ src/S322.cpp
@@ -2,27 +2,23 @@
 
 using namespace std;
 
-class Solution
-{
-public:
-    int coinChange(vector<int> &coins, int amount)
-    {
-        vector<int> dp(amount + 1, amount + 1);
+class Solution {
+   public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size(), MAX = amount + 2;
+        vector<int> dp(amount + 1, MAX);
         dp[0] = 0;
-        for (int i = 1; i <= amount; ++i)
-        {
-            for (int j = 0; j < coins.size(); ++j)
-            {
-                if (coins[j] <= i)
-                    dp[i] = min(dp[i], dp[i - coins[j]] + 1);
+        for (int i = 1; i <= amount; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i - coins[j] < 0) continue;
+                dp[i] = min(dp[i], dp[i - coins[j]] + 1);
             }
         }
-        return dp[amount] > amount ? -1 : dp[amount];
+        return dp[amount] == MAX ? -1 : dp[amount];
     }
 };
 
-int main()
-{
+int main() {
     vector<int> coins = {3, 7, 405, 436};
     int amount = 8839;
     cout << Solution().coinChange(coins, amount) << endl;
