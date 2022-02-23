7fc957c873eab8dfd75324f5c56b01d947d870b2
harmonly <bullniumao@outlook.com>
2022-02-23 10:44:12 +0800

    update S384.cpp

--- src/S384.cpp
+++ src/S384.cpp
@@ -4,40 +4,27 @@ using namespace std;
 
 class Solution {
    public:
-    Solution(vector<int>& nums) {
-        this->nums = nums;
-        this->original.resize(nums.size());
-        copy(nums.begin(), nums.end(), original.begin());
-    }
+    Solution(vector<int>& nums)
+        : nums(nums), original(nums), rng(random_device{}()) {}
 
-    vector<int> reset() {
-        copy(original.begin(), original.end(), nums.begin());
-        return nums;
-    }
+    vector<int> reset() { return original; }
 
     vector<int> shuffle() {
-        vector<int> shuffled = vector<int>(nums.size());
-        list<int> lst(nums.begin(), nums.end());
-        for (int i = 0; i < nums.size(); i++) {
-            int j = rand() % (lst.size());
-            auto it = lst.begin();
-            advance(it, j);
-            shuffled[i] = *it;
-            lst.erase(it);
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            int r = i + rand() % (n - i);
+            swap(nums[i], nums[r]);
         }
-        copy(shuffled.begin(), shuffled.end(), nums.begin());
         return nums;
     }
 
     vector<int> shuffle2() {
-        for (int i = 0; i < nums.size(); i++) {
-            int j = i + rand() % (nums.size() - i);
-            swap(nums[i], nums[j]);
-        }
+        std::shuffle(nums.begin(), nums.end(), rng);
         return nums;
     }
 
    private:
+    mt19937 rng;
     vector<int> nums;
     vector<int> original;
 };
