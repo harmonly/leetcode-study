883d32b0221cc1743e620d5370731e69cf3572e0
harmonly <bullniumao@outlook.com>
2022-02-26 15:23:39 +0800

    upload S2182.cpp

--- src/S2182.cpp
+++ src/S2182.cpp
@@ -5,25 +5,31 @@ using namespace std;
 class Solution {
    public:
     string repeatLimitedString(string s, int repeatLimit) {
-        vector<int> freq(26);
-        for (char c : s) freq[c - 'a'] += 1;
-        string ans;
-        int l = -1;
-        while (true) {
-            int done = 1;
-            int mx = -1;
-            for (int i = 25; i >= 0; i -= 1) {
-                if (freq[i] && mx == -1) mx = i;
-                if (freq[i] && i != l) {
-                    l = i;
-                    done = 0;
+        vector<int> freq(26, 0);    // 保存字符的个数
+        for (auto& ch : s) freq[ch - 'a']++;
+        string ans = "";
+        priority_queue<pair<int, int>> q;   // pair<字符, 字符个数>
+        for (int i = 25; i >= 0; --i)
+            if (freq[i]) q.push(make_pair(i, freq[i])); // 从后往前保存
+        while (!q.empty()) {    // 当还有字符则继续
+            auto p = q.top();
+            q.pop();
+            int c = p.first, cnt = p.second;    // 第一个要取的字符
+            if (cnt <= repeatLimit) {
+                for (int i = 0; i < cnt; ++i) ans += 'a' + c;
+            } else {
+                for (int i = 0; i < repeatLimit; ++i) ans += 'a' + c;
+                if (q.empty())  // 如果已经没有字符则退出
                     break;
+                else {
+                    auto np = q.top();  // 下一个字符
+                    q.pop();
+                    int nc = np.first, ncnt = np.second;
+                    ans += 'a' + nc;
+                    if (--ncnt) q.push(make_pair(nc, ncnt));
+                    q.push(make_pair(c, cnt - repeatLimit));
                 }
             }
-            if (done) break;
-            int x = min(freq[l], l == mx ? repeatLimit : 1);
-            freq[l] -= x;
-            for (int j = 0; j < x; j += 1) ans += 'a' + l;
         }
         return ans;
     }
