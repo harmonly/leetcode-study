b1e06ab201d0f02e3c4f1d8a16307d45dd0f82db
harmonly <bullniumao@outlook.com>
2022-02-27 16:48:47 +0800

    upload S217.cpp

---
+++ src/S217.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+   public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> st;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (st.count(nums[i])) return true;
+            st.insert(nums[i]);
+        }
+        return false;
+    }
+};
