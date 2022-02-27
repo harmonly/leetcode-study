49d407d7ddf8ad8667705acbc67b2e17c5970364
harmonly <bullniumao@outlook.com>
2022-02-27 17:47:47 +0800

    update S220.cpp

--- src/S220.cpp
+++ src/S220.cpp
@@ -10,9 +10,9 @@ class Solution {
         for (int i = 0; i < n; ++i) {
             if (i > k) ms.erase(nums[i - k - 1]);
             auto it = ms.lower_bound(max(nums[i], INT_MIN + t) -
-                                     t);  // 寻找 y >= [x - t]
+                                     t);  // 最小不能小于 INT_MIN
             if (it != ms.end() && *it <= min(nums[i], INT_MAX - t) + t)
-                return true;  // 寻找 y <= [x + t]
+                return true;  // 最大不能大于 INT_MAX
             ms.insert(nums[i]);
         }
         return false;
