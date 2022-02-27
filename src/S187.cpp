30cd0440f5a312a067df9bdf871bf10293d5cc42
harmonly <bullniumao@outlook.com>
2022-02-27 16:34:28 +0800

    update S187.cpp

--- src/S187.cpp
+++ src/S187.cpp
@@ -14,4 +14,25 @@ class Solution {
         }
         return ans;
     }
+
+    unordered_map<char, int> bin = {{'A', 0}, {'C', 1}, {'G', 2}, {'T', 3}};
+    vector<string> findRepeatedDnaSequences2(string s) {
+        vector<string> ans;
+        int n = s.length();
+        if (n <= 10) return ans;
+        int x = 0;
+        for (int i = 0; i < 9; ++i) x = (x << 2) | bin[s[i]];
+        unordered_map<int, int> cnt;
+        for (int i = 0; i <= n - 10; ++i) {
+            x = ((x << 2) | bin[s[i + 9]]) & ((1 << 20) - 1);
+            if (++cnt[x] == 2) ans.push_back(s.substr(i, 10));
+        }
+        return ans;
+    }
 };
+
+int main() {
+    string s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
+    Solution().findRepeatedDnaSequences2(s);
+    return 0;
+}
