249bc4e1f718e7f12990a8fb08ecac85991f4ba1
harmonly <bullniumao@outlook.com>
2022-03-11 16:11:08 +0800

    upload S2049.cpp

--- src/S2049.cpp
+++ src/S2049.cpp
@@ -37,3 +37,45 @@ class Solution {
         return size;
     }
 };
+
+class Solution2 {
+    int l[100001], r[100001], size[100001], n;
+
+   public:
+    int countHighestScoreNodes(vector<int>& parents) {
+        this->n = parents.size();
+        for (int i = 1; i < n; ++i) {
+            int p = parents[i];
+            if (!l[p])
+                l[p] = i;
+            else
+                r[p] = i;
+        }
+        dfs(0);
+        long long maxScore = 0;
+        int cnt = 0;
+        for (int i = 0; i < n; ++i) {
+            int a = 1, b = 1, c = 1;
+            if (i != 0) a = n - size[i];
+            if (l[i]) b = size[l[i]];
+            if (r[i]) c = size[r[i]];
+            if (1LL * a * b * c > maxScore)
+                maxScore = 1LL * a * b * c, cnt = 1;
+            else if (1LL * a * b * c == maxScore)
+                ++cnt;
+        }
+        return cnt;
+    }
+
+    void dfs(int x) {
+        size[x] = 1;
+        if (l[x]) {
+            dfs(l[x]);
+            size[x] += size[l[x]];
+        }
+        if (r[x]) {
+            dfs(r[x]);
+            size[x] += size[r[x]];
+        }
+    }
+};
