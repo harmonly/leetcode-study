22525f8f764be0503d1b33b9a5193da5d779cbc2
harmonly <bullniumao@outlook.com>
2022-03-12 16:49:42 +0800

    update S541.cpp

--- src/S451.cpp
+++ src/S451.cpp
@@ -2,34 +2,48 @@
 
 using namespace std;
 
-class Solution
-{
-public:
-    string frequencySort(string s)
-    {
+class Solution {
+   public:
+    string frequencySort(string s) {
         unordered_map<char, int> map;
         int n = s.length();
-        for (auto &ch : s)
-            map[ch]++;
+        for (auto &ch : s) map[ch]++;
         vector<pair<char, int>> vec;
-        for (auto &it : map)
-            vec.emplace_back(it);
-        sort(vec.begin(), vec.end(), [](const pair<char, int> &a, const pair<char, int> &b)
-             { return a.second > b.second; });
+        for (auto &it : map) vec.emplace_back(it);
+        sort(vec.begin(), vec.end(),
+             [](const pair<char, int> &a, const pair<char, int> &b) {
+                 return a.second > b.second;
+             });
         string ret;
         for (auto &it : vec)
-            for (int i = 0; i < it.second; i++)
-                ret.push_back(it.first);
+            for (int i = 0; i < it.second; i++) ret.push_back(it.first);
         return ret;
     }
 
-    string frequencySort2(string s)
-    {
+    string frequencySort2(string s) {
         unordered_map<char, int> map;
-        for (auto &ch : s)
-            map[ch]++;
-        sort(s.begin(), s.end(), [&](const char &a, const char &b)
-             { return map[a] == map[b] ? a > b : map[a] > map[b]; });
+        for (auto &ch : s) map[ch]++;
+        sort(s.begin(), s.end(), [&](const char &a, const char &b) {
+            return map[a] == map[b] ? a > b : map[a] > map[b];
+        });
         return s;
+    }
+};
+
+class Solution2 {
+   public:
+    string frequencySort(string s) {
+        unordered_map<char, int> mp;
+        int maxFreq = 0, n = s.length();
+        for (auto &c : s) maxFreq = max(maxFreq, ++mp[c]);
+        vector<string> buckets(maxFreq + 1);
+        for (auto &it : mp) buckets[it.second].push_back(it.first);
+        string ans;
+        for (int i = maxFreq; i > 0; --i) {
+            string &bucket = buckets[i];
+            for (auto &c : bucket)
+                for (int k = 0; k < i; ++k) ans.push_back(c);
+        }
+        return ans;
     }
 };
