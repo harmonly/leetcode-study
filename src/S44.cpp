e5d060b360a022212d7b9748e0820347ce3eef12
harmonly <bullniumao@outlook.com>
2022-03-18 18:20:03 +0800

    upload S44.cpp

---
+++ src/S44.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+    int dp[2000][2000];
+
+   public:
+    bool isMatch(string s, string p) {
+        int m = s.length(), n = p.length();
+        dp[0][0] = 1;
+        for (int i = 0; i <= m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (dp[i][j]) {
+                    if (p[j] == '?') dp[i + 1][j + 1] = 1;
+                    if (isalpha(p[j]) && s[i] == p[j]) dp[i + 1][j + 1] = 1;
+                    if (p[j] == '*') dp[i + 1][j] = 1, dp[i][j + 1] = 1;
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
