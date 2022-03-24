0ee5631d01950dd3a904d11f69774e2a282d35c4
harmonly <bullniumao@outlook.com>
2022-03-24 10:43:13 +0800

    update S55.cpp

--- src/S55.cpp
+++ src/S55.cpp
@@ -28,3 +28,19 @@ class Solution {
         return false;
     }
 };
+
+class Solution2 {
+   public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size(), l = 0, r = 0;  // [l, r] 表示跳 i 步的范围
+        if (n == 1) return true;
+        for (int i = 1; i < n; ++i) {  // i 表示跳跃次数, 范围 [1, n - 1]
+            int m = 0;                 // 最远跳的位置
+            for (int j = l; j <= r; ++j) m = max(m, nums[j] + j);  // 更新 m
+            if (m >= n - 1) return true;  // 如果跳到了最后一位, 则成功
+            l = r + 1;                    // 更新左边界
+            r = m;                        // 更新右边界
+        }
+        return false;
+    }
+};
