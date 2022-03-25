3335491d7351dd68e8aa1c886eeb76a3532ef26b
harmonly <bullniumao@outlook.com>
2022-03-25 13:31:48 +0800

    upload S172.cpp

---
+++ src/S172.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+   public:
+    int trailingZeroes(int n) {
+        int ans = 0;
+        while (n) {
+            n /= 5;
+            ans += n;
+        }
+        return ans;
+    }
+};
