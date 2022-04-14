1231a505d98010ae6981431f06bb68f6f7c146e5
harmonly <bullniumao@outlook.com>
2022-04-14 16:58:36 +0800

    upload S1672.cpp

---
+++ src/S1672.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+   public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int ans = 0;
+        for (auto& account : accounts)
+            ans = max(ans, accumulate(account.begin(), account.end(), 0));
+        return ans;
+    }
+};
