b6d12493ae852cb51b340db582588717be9fc54f
harmonly <bullniumao@outlook.com>
2022-04-24 13:39:28 +0800

    upload S868.cpp

---
+++ src/S868.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+   public:
+    int binaryGap(int n) {
+        int last = -1, ans = 0;
+        for (int i = 0; n; ++i) {
+            if (n & 1) {
+                if (last != -1) ans = max(ans, i - last);
+                last = i;
+            }
+            n >>= 1;
+        }
+        return ans;
+    }
+};
