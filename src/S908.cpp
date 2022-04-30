4d94b67c2b204d7b2d4a27b1290da2c1de99c551
harmonly <bullniumao@outlook.com>
2022-04-30 09:45:30 +0800

    upload S908.cpp

---
+++ src/S908.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+   public:
+    int smallestRangeI(vector<int>& nums, int k) {
+        return max(*max_element(nums.begin(), nums.end()) -
+                       *min_element(nums.begin(), nums.end()) - 2 * k,
+                   0);
+    }
+};
